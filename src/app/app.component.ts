786d0d7d2b341f3c6b9f627cb07b75a5137d0087
Clayton Wakefield <clayton.wakefield@opc.gov.au>
2019-05-01 10:14:01 +1000

    kendo sortable 2.1 example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { SortableComponent } from '@progress/kendo-angular-sortable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app';
+
+  @ViewChild(SortableComponent) sortable: SortableComponent;
+  itemsA = [];
+
+  itemsB = [];
+
+  counter = 0;
+
+  newItem() {
+    const newItem = {
+      name: `Item ${this.counter++}`
+    };
+    this.sortable.addDataItem(newItem, this.itemsA.length);
+  }
 }
