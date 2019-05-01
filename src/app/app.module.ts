462ed3724e267d07565d990c7f74e9c499cc3db7
Clayton Wakefield <clayton.wakefield@opc.gov.au>
2019-05-01 10:13:41 +1000

    add kendo sortable package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SortableModule } from '@progress/kendo-angular-sortable';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, SortableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
